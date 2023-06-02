196c2a3faf91f1867c104082d089458495729141
mithi17 <mithileshkumar170203@gmail.com>
2023-06-02 15:03:13 +0530

    macchanger.py modified

--- macchanger.py
+++ macchanger.py
@@ -1,3 +1,22 @@
 import subprocess
+import optparse
 
-config = input("enter the network> ")
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change it's mac address")
+    parse.add_option("-m", "--mac", dest="new_mac", help="new mac address")
+    (options, arguments) = parser.prase_args()
+    if not options.interface:
+        parser.error("[-] please specify a interface, use --help for more options")
+    elif not options.new_mac:
+        parser.error("[-] please specify a new mac address, use --help for more options")
+    return options
+
+def change_mac(interface,new_mac ):
+    print("[+]changing mac address for" + interface + "to" + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", + new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
