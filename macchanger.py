4e0edfcb1e75e3b4318867c44d8e24f390b15edc
root <root@kali.kali.org>
2023-06-06 10:40:33 -0400

    file modified

--- macchanger.py
+++ macchanger.py
@@ -4,7 +4,7 @@ import optparse
 def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change it's mac address")
-    parse.add_option("-m", "--mac", dest="new_mac", help="new mac address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="new mac address")
     (options, arguments) = parser.prase_args()
     if not options.interface:
         parser.error("[-] please specify a interface, use --help for more options")
@@ -12,11 +12,11 @@ def get_arguments():
         parser.error("[-] please specify a new mac address, use --help for more options")
     return options
 
-def change_mac(interface,new_mac ):
+def change_mac(interface, new_mac):
     print("[+]changing mac address for" + interface + "to" + new_mac)
-    subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ether", + new_mac])
-    subprocess.call(["ifconfig", interface, "up"])
-
+    subprocess.call(["sudo", "ifconfig", interface, "down"])
+    subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", + new_mac])
+    subprocess.call(["sudo", "ifconfig", interface, "up"])
+    
 options = get_arguments()
 change_mac(options.interface, options.new_mac)
